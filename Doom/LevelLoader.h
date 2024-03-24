c6ccbfc391adaa301a9100ac12f64ecfeaf4670d
antiqueMouse <92105863+StrayDog31@users.noreply.github.com>
2024-03-25 00:55:16 +0300

    Add files via upload

---
+++ Doom/LevelLoader.h
@@ -0,0 +1,16 @@
+//
+// Created by Yakovlev Sergey on 13.03.2024.
+//
+
+#ifndef DOOM_LEVELLOADER_H
+#define DOOM_LEVELLOADER_H
+
+
+class LevelLoader {
+
+public:
+    static void levelOpener(const char* file_name);
+};
+
+
+#endif //DOOM_LEVELLOADER_H
